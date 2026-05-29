5a469367346d1c9a982c4e51f65a94057f27a194
Aloecheverria7 <33984928+Aloecheverria7@users.noreply.github.com>
2026-05-29 08:21:06 -0600

    test
    test

--- server/src/utils/http.js
+++
@@ -1,7 +0,0 @@
-export function asyncHandler(handler) {
-  return (req, res, next) => Promise.resolve(handler(req, res, next)).catch(next);
-}
-
-export function sendCreated(res, data) {
-  res.status(201).json(data);
-}
